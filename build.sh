f821bc63b7204b6dbfb7272a1c8b6deea92435f8
Ihor <ihor.mudryy@here.com>
2018-11-15 18:40:19 +0100

    Moved clean script to build as a function

--- build.sh
+++ build.sh
@@ -3,9 +3,31 @@ export FABRIC_CFG_PATH=$PWD
 export COIN_NAME="mudryycoin"
 export COIN_NAME_CAMELCASE="MudryyCoin"
 
-bash ./clean.sh
+clean(){
+    docker rm -f $(docker ps -aq)
+    images=(blockchain-setup cryptocurrency-ca shop-ca orderer-peer cryptocurrency-peer shop-peer backend redis-server rabbit-client)
+    for i in "${images[@]}"
+    do
+        echo Removing image : $i
+      docker rmi -f $i
+    done
+
+    #docker rmi -f $(docker images | grep none)
+    images=(dev-shop-peer dev-cryptocurrency-peer)
+    for i in "${images[@]}"
+    do
+        echo Removing image : $i
+      docker rmi -f $(docker images | grep $i )
+    done
+
+    docker rmi $(docker images -f "dangling=true" -q)
+}
+
+clean
+
 bash ./generate-certs.sh
 bash ./docker-images.sh
+
 docker-compose -p "${COIN_NAME}" up -d cryptocurrency-peer
 sleep 20s
 docker-compose -p "${COIN_NAME}" up -d blockchain-setup
